38b5063e78653fcc1b505177881058fb431b54e0
KwMateusz <kwidzinski.mateusz.develop@gmail.com>
2020-06-11 21:09:55 +0200

    Added card displey.

--- src/utils/card.rs
+++ src/utils/card.rs
@@ -1,4 +1,5 @@
 use enum_iterator::IntoEnumIterator;
+use std::fmt;
 
 #[derive(Debug, PartialEq, Copy, Clone, IntoEnumIterator)]
 pub enum Suit {
@@ -30,4 +31,38 @@ pub struct Card {
     pub suit: Suit,
     pub figure: Figure,
 }
-// TODO: implement Display
+
+impl fmt::Display for Figure {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Figure::Ace => write!(f, "A"),
+            Figure::Two => write!(f, "2"),
+            Figure::Three => write!(f, "3"),
+            Figure::Four => write!(f, "4"),
+            Figure::Five => write!(f, "5"),
+            Figure::Six => write!(f, "6"),
+            Figure::Seven => write!(f, "7"),
+            Figure::Eight => write!(f, "8"),
+            Figure::Nine => write!(f, "9"),
+            Figure::Ten => write!(f, "10"),
+            Figure::Jack => write!(f, "J"),
+            Figure::Queen => write!(f, "Q"),
+            Figure::King => write!(f, "K"),
+        }
+    }
+}
+impl fmt::Display for Suit {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Suit::Spades => write!(f, "♠"),
+            Suit::Hearts => write!(f, "♥"),
+            Suit::Diamonds => write!(f, "♦"),
+            Suit::Clubs => write!(f, "♣"),
+        }
+    }
+}
+impl fmt::Display for Card {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} {}", self.figure, self.suit)
+    }
+}
